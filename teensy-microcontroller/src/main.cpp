0b9bdd377dfcec4f9f64fc990539792ccaa7c3b1
Bhajneet Singh Bedi <92092790+Bhajneet-Singh-Bedi@users.noreply.github.com>
2024-08-22 00:24:30 +0530

    SPI communication code

--- teensy-microcontroller/src/main.cpp
+++ teensy-microcontroller/src/main.cpp
@@ -1,5 +1,13 @@
 #include <Encoder.h>
 #include <PID_v1.h>
+#include <SPI.h>
+
+#define head1 0xAA
+#define head2 0x55
+#define sendType_velocity 0x11
+#define sendType_pid 0x12
+#define sendType_params 0x13
+
 // mot-1 conn with pwm pin
 const int motor1_pin1 = 9;
 const int motor1_pin2 = 10;
@@ -16,6 +24,21 @@ const int ENC1B = 3;
 const int ENC2A = 4;
 const int ENC2B = 5;
 
+const int ss = 8;
+
+double encPos1 = 0;
+double encPos2 = 0;
+
+double lastEncPos1 = 0;
+double lastEncPos2 = 0;
+
+double prevT1 = 0;
+double prevT2 = 0;
+
+float WHEELBASE = 20.0; // in m
+float WHEELDIA = 0.1;   // in m
+float encRES = 1000.0;
+
 Encoder motor1Encoder(ENC1A, ENC1B);
 Encoder motor2Encoder(ENC2A, ENC2B);
 
@@ -30,24 +53,9 @@ PID pid2(&input2, &output2, &setpoint2, kp, ki, kd, DIRECT);
 
 class BOT
 {
-private:
-  long encPos1 = 0;
-  long encPos2 = 0;
-
-  long lastEncPos1 = 0;
-  long lastEncPos2 = 0;
-
-  unsigned long prevT1 = 0;
-  unsigned long prevT2 = 0;
-
-  float WHEELBASE = 20.0; // in m
-  float WHEELDIA = 0.1;   // in m
-  float encRES = 1000.0;
-
 public:
   void setup()
   {
-    // read pos from encoder.
     pinMode(ENC1A, INPUT);
     pinMode(ENC1B, INPUT);
     pinMode(ENC2A, INPUT);
@@ -65,38 +73,42 @@ public:
 
     pid1.SetMode(AUTOMATIC);
     pid2.SetMode(AUTOMATIC);
+
+    pinMode(ss, OUTPUT);
+    digitalWrite(ss, LOW);
+    SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
   }
 
-  long getCurrentSpeedLinear() { return (speed1() + speed2()) / 2; }                                               // m/s
-  long getCurrentRotationSpeed() { return ((speed2() / (WHEELDIA / 2)) - (speed1() / WHEELDIA / 2)) / WHEELBASE; } // rad/sec
+  double getCurrentSpeedLinear() { return (speed1() + speed2()) / 2; }                                               // m/s
+  double getCurrentRotationSpeed() { return ((speed2() / (WHEELDIA / 2)) - (speed1() / WHEELDIA / 2)) / WHEELBASE; } // rad/sec
 
-  long speed1()
+  double speed1()
   {
-    unsigned long currT = millis();
-    unsigned long delT = (currT - prevT1) / 1000.0;
+    double currT = millis();
+    double delT = (currT - prevT1) / 1000.0;
 
     encPos1 = motor1Encoder.read();
-    long encSpeed1 = (encPos1 - lastEncPos1) / delT;
-    long rps1 = encSpeed1 / encRES; // rps.
+    double encSpeed1 = (encPos1 - lastEncPos1) / delT;
+    double rps1 = encSpeed1 / encRES; // rps.
     lastEncPos1 = encPos1;
     return rps1 * WHEELDIA * PI; // speed (m/s)
   }
 
-  long speed2()
+  double speed2()
   {
-    unsigned long currT = millis();
-    unsigned long delT = (currT - prevT2) / 1000.0;
+    double currT = millis();
+    double delT = (currT - prevT2) / 1000.0;
 
     encPos2 = motor2Encoder.read();
-    long encSpeed2 = (encPos2 - lastEncPos2) * delT;
-    long rps2 = encSpeed2 / encRES; // ros.
+    double encSpeed2 = (encPos2 - lastEncPos2) * delT;
+    double rps2 = encSpeed2 / encRES; // ros.
     lastEncPos2 = encPos2;
     return rps2 * WHEELDIA * PI; // speed (m/s)
   }
 
-  void setSpeed(float x, float y)
+  void setSpeed(float x, float yaw)
   {
-    // x is linear speed, y is angular speed
+    // x is linear speed, yaw is angular speed
     // V-linear, omega-angular, L-WHEELBASE, r-wheelRad
     // linear velocity of wheel
     // Vr = V + (omega * L)/2
@@ -110,8 +122,8 @@ public:
     // rpsR = ThetaR/2*PI;
     // rpsL = ThetaL/2*PI;
 
-    float Vr = x + (y * WHEELBASE) / 2;
-    float Vl = x - (y * WHEELBASE) / 2;
+    float Vr = x + (yaw * WHEELBASE) / 2;
+    float Vl = x - (yaw * WHEELBASE) / 2;
 
     float ThetaR = Vr / (WHEELDIA / 2);
     float ThetaL = Vl / (WHEELDIA / 2);
@@ -119,10 +131,10 @@ public:
     float rpsR = ThetaR / 2 * PI;
     float rpsL = ThetaL / 2 * PI;
 
-    setMPS(rpsR, rpsL);
+    sebufferS(rpsR, rpsL);
   }
 
-  void setMPS(float rpsR, float rpsL)
+  void sebufferS(float rpsR, float rpsL)
   {
     input1 = speed1() / (WHEELDIA * PI); // in rps
     input2 = speed2() / (WHEELDIA * PI); // in rps
@@ -140,13 +152,13 @@ public:
     {
       digitalWrite(motor1_pin1, HIGH);
       digitalWrite(motor1_pin2, LOW);
-      analogWrite(pwm_1, speed);
+      analogWrite(pwm_1, abs(speed));
     }
     else if (speed < 0)
     {
       digitalWrite(motor1_pin1, LOW);
       digitalWrite(motor1_pin2, HIGH);
-      analogWrite(pwm_1, -speed);
+      analogWrite(pwm_1, abs(speed));
     }
     else
     {
@@ -179,6 +191,17 @@ public:
   }
 };
 
+uint8_t checksum(uint8_t *buf, size_t len)
+{
+  uint8_t sum = 0x00;
+  for (int i = 0; i < len; i++)
+  {
+    sum += *(buf + i);
+  }
+
+  return sum;
+}
+
 BOT bot;
 void setup()
 {
@@ -187,7 +210,23 @@ void setup()
 
 void loop()
 {
-  bot.getCurrentSpeedLinear();
-  bot.getCurrentRotationSpeed();
-  bot.setSpeed(3.0, 3);
+  static uint8_t buffer[9];
+
+  double x = bot.getCurrentSpeedLinear();
+  double yaw = bot.getCurrentRotationSpeed();
+  // digitalWrite(ss, LOW);
+  buffer[0] = head1;
+  buffer[1] = head2;
+  buffer[2] = 0x09;
+  buffer[3] = sendType_velocity;
+  buffer[4] = ((int16_t)(x * 1000) >> 8) & 0xff;
+  buffer[5] = ((int16_t)(x * 1000)) & 0xff;
+  buffer[6] = ((int16_t)(yaw * 1000) >> 8) & 0xff;
+  buffer[7] = ((int16_t)(yaw * 1000)) & 0xff;
+  buffer[8] = checksum(buffer, 9);
+  SPI.transfer(buffer, 9);
+
+  // Same for this one.
+  bot.setSpeed(x, yaw);
+  // digitalWrite(ss, HIGH);
 }
